bb3d62d2a0e2a95e385118979bf979ca712903f3
ferdyirawanf <ferdyirawanf@gmail.com>
2021-03-26 02:59:08 +0700

    fix elmnt species skill breeding

--- Breeding/Breeding.hpp
+++ Breeding/Breeding.hpp
@@ -15,11 +15,11 @@ using namespace std;
 
 class Breeding {
     public:
-        EngimonPlayer& makeBreeding(EngimonPlayer*, EngimonPlayer*);
-        int getRandom(int);
+        EngimonPlayer* makeBreeding(EngimonPlayer*, EngimonPlayer*);
+        //int getRandom(int);
         void setParentName(EngimonPlayer*, EngimonPlayer*, EngimonPlayer*);
-        void setElement(EngimonPlayer*, EngimonPlayer*, EngimonPlayer*);
-        void setSpecies(EngimonPlayer*, EngimonPlayer*, EngimonPlayer*);
+        void setElementSpecies(EngimonPlayer*, EngimonPlayer*, EngimonPlayer*);
+        //void setSpecies(EngimonPlayer*, EngimonPlayer*, EngimonPlayer*);
         void inheritSkill(EngimonPlayer*, EngimonPlayer*, EngimonPlayer*);
 };
 #endif;
